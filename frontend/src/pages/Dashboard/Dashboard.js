e73d35aab77f2bdaac53ec7067efd783c11064ef
JoanG5 <Joanguzman553@gmail.com>
2024-02-12 20:57:20 -0500

    set up tailwind css and daisyui

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 function Dashboard() {
-  return (
-    <h1>Dashboard</h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Dashboard</h1>;
 }
 
-export default Dashboard
+export default Dashboard;
