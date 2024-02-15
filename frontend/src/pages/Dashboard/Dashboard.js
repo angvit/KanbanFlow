e3f4d55da45e977fc310162f59530bc34e073391
JoanG5 <Joanguzman553@gmail.com>
2024-02-14 23:29:55 -0500

    added sidebar into Dashboard

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -4,10 +4,34 @@ import DashboardContainer from "../../components/DashboardContainer/DashboardCon
 function Dashboard() {
   return (
     <>
-      <div className="flex">
-        <DashboardContainer title="To do" />
-        <DashboardContainer title="Doing" />
-        <DashboardContainer title="Done" />
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content items-center justify-center">
+          <div className="flex">
+            <DashboardContainer title="To do" />
+            <DashboardContainer title="Doing" />
+            <DashboardContainer title="Done" />
+          </div>
+        </div>
+        <div className="drawer-side">
+          <ul className="menu p-4 w-72 min-h-full bg-base-200 text-base-content">
+            <li>
+              <button className="btn btn-ghost justify-start text-lg">
+                Boards
+              </button>
+            </li>
+            <li>
+              <button className="btn btn-ghost justify-start text-lg">
+                Members
+              </button>
+            </li>
+            <li>
+              <button className="btn btn-ghost justify-start text-lg">
+                Workspace Settings
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
