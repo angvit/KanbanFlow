07a13ce316745c0c7d60459e2352795c1dcfaa1e
Joan <Joanguzman553@gmail.com>
2024-02-15 15:42:29 -0500

    made the task into buttons to make them into modals

--- frontend/src/components/TaskCard/TaskCard.js
+++ frontend/src/components/TaskCard/TaskCard.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function TaskCard(props) {
   return (
-    <div className="card bg-base-100 shadow-xl">
-      <div className="card-body">
-        <h2 className="card-title">{props.title}</h2>
-        <p>{props.task}</p>
-      </div>
+    <div className="card bg-base-200">
+      <button className="h-full w-full btn btn-neutral">
+        <div className="card-body">
+          <h2 className="card-title">{props.title}</h2>
+          <p className="card-title text-lg">{props.task}</p>
+        </div>
+      </button>
     </div>
   );
 }
