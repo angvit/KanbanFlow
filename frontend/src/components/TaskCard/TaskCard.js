39b2e8d70d1b61eea7ec37cb6f923b59eeb91c82
Joan <Joanguzman553@gmail.com>
2024-02-15 16:10:27 -0500

    added modal to the Taskcards so when you click them you should be able to edit them

--- frontend/src/components/TaskCard/TaskCard.js
+++ frontend/src/components/TaskCard/TaskCard.js
@@ -2,14 +2,30 @@ import React from "react";
 
 function TaskCard(props) {
   return (
-    <div className="card bg-base-200">
-      <button className="h-full w-full btn btn-neutral">
-        <div className="card-body">
-          <h2 className="card-title">{props.title}</h2>
-          <p className="card-title text-lg">{props.task}</p>
+    <>
+      <div className="card bg-base-200">
+        <button
+          className="h-full w-full btn btn-neutral"
+          onClick={() => document.getElementById("my_modal_1").showModal()}
+        >
+          <div className="card-body">
+            <h2 className="card-title">{props.title}</h2>
+            <p className="card-title text-lg">{props.task}</p>
+          </div>
+        </button>
+      </div>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box w-full h-full">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Placeholder For Task Editor</h3>
+          <p className="py-4">Placeholder for Task Editor options</p>
         </div>
-      </button>
-    </div>
+      </dialog>
+    </>
   );
 }
 
