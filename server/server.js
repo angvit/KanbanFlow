3c33a9741d1a188eb68d17ad8b96147074c26dea
JoanG5 <Joanguzman553@gmail.com>
2024-02-22 18:01:09 -0500

    changed named of front end folder to client

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
 });
 
 app.get("/", (req, res) => {
