d56161e8ee5de2bc014cebe2591456a52f98d410
JoanG5 <Joanguzman553@gmail.com>
2024-02-22 18:15:47 -0500

    fixing files

---
+++ client/src/pages/Boards/BoardSideBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import BoardSideBar from "./BoardSideBar";
+import BoardMidContent from "./BoardMidContent";
+import "./Board.css";
+
+export default function Board() {
+  return (
+    <div className="kanban-root">
+      <div className="browser-container">
+        <div className="surface">
+          <main className="content-boundary">
+            <div className="content-wrapper">
+              <div className="board-left-side-content">
+                <BoardSideBar />
+              </div>
+              <div className="board-middle-content">
+                <BoardMidContent />
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
