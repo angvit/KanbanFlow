2cf5c4726da0982d2761050659e75f875d7310b3
noshin7 <noshin7201@gmail.com>
2024-03-01 21:34:29 -0500

    Added profile

---
+++ frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,9 @@
+function saveInfo() {
+    var username = document.getElementById('username').value;
+    var bio = document.getElementById('bio').value;
+
+    console.log('Username:', username);
+    console.log('Bio:', bio);
+
+    alert('Information saved successfully!');
+}
