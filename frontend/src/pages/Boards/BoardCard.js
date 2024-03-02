559d2a56558a5d49354f6c7d8e11f949760e6c66
angvit <angelo.vitalino@baruchmail.cuny.edu>
2024-03-02 00:24:58 -0500

    Adding BoardCards functionality

--- frontend/src/pages/Boards/BoardCard.js
+++ frontend/src/pages/Boards/BoardCard.js
@@ -3,11 +3,14 @@ import '../../pages/Boards/Board.css'
 
 
 const BoardCard = ({ title, onClick }) => {
+
+    console.log(title);
+
     return (
         <div onClick={onClick} className='cursor-pointer'>
-            <div className="card w-50 bg-base-200 shadow-lg board-card">
+            <div className="card w-50 bg-base-200 shadow-lg board-card ">
                 <div className="card-body board-card-body">
-                    <h2 className="card-title">{title || 'Untitled Board'}</h2>
+                    <h2 className="card-title">{title}</h2>
                 </div>
             </div>
         </div>
