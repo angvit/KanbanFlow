8cb2942c3ff9d4c5963d54eef5c2d3b5093b8e6f
JoanG5 <Joanguzman553@gmail.com>
2024-03-15 23:10:07 -0400

    added navigate to the add board function

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -78,7 +78,6 @@ function Navbar() {
       name: workspaceName,
       description: workspaceDescription,
     };
-
     axios
       .post(`workspaces/${user.sub}`, workspaceData)
       .then((response) => {
